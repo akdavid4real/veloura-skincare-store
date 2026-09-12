f083b5c52782a27373afece97e0b1a0ac6d0fa91
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-12 18:29:40 +0100

    feat: route admin workspace in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminApp from './admin/AdminApp.jsx'
 import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+const isAdmin = window.location.pathname.startsWith('/admin')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>{isAdmin ? <AdminApp /> : <App />}</React.StrictMode>
+)
